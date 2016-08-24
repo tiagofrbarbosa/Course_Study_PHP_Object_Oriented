05654b42c67aed6b41fcbb3f0601069d594b79f1
tiagofrbarbosa <tiagofr.barbosa@hotmail.com.br>
2016-08-24 08:34:16 -0300

    adicionando tabela de usuários

--- mysql/mysql.sql
+++ mysql/mysql.sql
@@ -3,3 +3,7 @@ create table produtos (id integer auto_increment primary key, nome varchar(255),
 create table categorias (id integer auto_increment primary key, nome varchar(255));
 
 insert into categorias (nome) values ("esporte"), ("escola"), ("mobilidade"), ("guloseimas");
+
+create table usuarios (id integer auto_increment primary key, email varchar(255), senha varchar(255));
+
+insert into usuarios (email,senha) values('tiagofr.barbosa@hotmail.com.br','e10adc3949ba59abbe56e057f20f883e');
